5889769c0c4ac7f3c478b5c7bed8fa95cd050ec6
AndreSathiavan <78586626+AndreSathiavan@users.noreply.github.com>
2022-01-23 14:30:43 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ function preload() {
   explosionImg = loadImage("explosion.png");
   restartImg = loadImage("restart.png");
   backgroundImg2 = loadImage("back.jpeg");
-  backgroundImg = loadImage("Background.png");
+  backgroundImg = loadImage("background.jpeg");
   playerimage = loadImage("GoodGuysSpaceship2.png");
   computerimage = loadImage("BadGuysSpaceShip.png");
   alienImg = loadImage("BadGuys.png")
